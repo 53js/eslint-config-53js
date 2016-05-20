2883551e0baced6526af2f97906a6fd490a5775f
Hugo Mallet <hugo.mallet@53js.fr>
2016-05-20 16:58:41 +0200

    modified rules

--- rules/53js.js
+++ rules/53js.js
@@ -1,12 +1,10 @@
 module.exports = {
 	rules: {
 		'indent': ['error', 'tab'],
-		'spaced-comment': [1, 'always', {
-			'exceptions': ['-', '+'],
-			'markers': ['=', '!', '/'] // space here to support sprockets directives
-		}],
-		// 'vars-on-top': 0,
+		'spaced-comment': 0,
+		//'vars-on-top': 0, // Prevent lisibiliy
 		'no-underscore-dangle': [1, { 'allowAfterThis': true }],
-		'wrap-iife': ['error', 'any']
+		'wrap-iife': ['error', 'any'],
+		'no-param-reassign': 0 // Reassigning params is sometimes what we want
 	}
 };
