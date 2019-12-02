4199e8b3da6f3d08657f8994ccceb5153f7d0656
Hugo Mallet <hugo.mallet@53js.fr>
2019-12-02 11:19:31 +0100

    feat: update react rules +hooks +updated deps +yarn

--- react/react.js
+++ react/react.js
@@ -2,11 +2,15 @@ module.exports = {
 	extends: [
 		'eslint-config-airbnb-base/rules/strict',
 		'eslint-config-airbnb/rules/react',
+		'eslint-config-airbnb/rules/react-hooks',
 	].map(require.resolve),
+	parser: 'babel-eslint',
 	rules: {
 		'react/jsx-filename-extension': [2, { extensions: ['.js'] }],
 		'react/jsx-indent': [2, 'tab'],
 		'react/jsx-indent-props': [2, 'tab'],
-		'react/prefer-stateless-function': 1
+		'react/jsx-props-no-spreading': 'off',
+		'react/prefer-stateless-function': 1,
+		'react/state-in-constructor': 'off',
 	}
 };
