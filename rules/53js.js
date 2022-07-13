a69d499200f9cf4d0ca1136bed1a8e954bc67b06
Leo53JS <leo.guillaumet@53js.fr>
2022-07-13 14:00:26 +0200

    feat(rules): add warning for semi

--- rules/53js.js
+++ rules/53js.js
@@ -1,16 +1,17 @@
 module.exports = {
 	rules: {
-		'no-tabs': 0,
-		indent: [2, 'tab'],
-		'spaced-comment': 0,
-		'no-param-reassign': 0, // Reassigning params is sometimes what we want
 		'function-paren-newline': ['error', 'consistent'],
+		indent: [2, 'tab'],
 		'max-len': ['warn', 100, 2, {
-			ignoreUrls: true,
 			ignoreComments: false,
 			ignoreRegExpLiterals: true,
 			ignoreStrings: true,
 			ignoreTemplateLiterals: true,
+			ignoreUrls: true
 		}],
+		'no-param-reassign': 0, // Reassigning params is sometimes what we want
+		'no-tabs': 0,
+		semi: 'warn',
+		'spaced-comment': 0
 	}
 };
