b60e10adc769ba4cd0a0372f53f40e42ecbfa9a7
Leo53JS <leo.guillaumet@53js.fr>
2022-07-13 11:51:03 +0200

    feat(rules): config max-len rule

--- rules/53js.js
+++ rules/53js.js
@@ -5,5 +5,12 @@ module.exports = {
 		'spaced-comment': 0,
 		'no-param-reassign': 0, // Reassigning params is sometimes what we want
 		'function-paren-newline': ['error', 'consistent'],
+		'max-len': ['warn', 100, 2, {
+			ignoreUrls: true,
+			ignoreComments: false,
+			ignoreRegExpLiterals: true,
+			ignoreStrings: true,
+			ignoreTemplateLiterals: true,
+		}],
 	}
 };
